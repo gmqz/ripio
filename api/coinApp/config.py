52cafb80eaface7d81ee94ab0c2e78b19b2785f2
mqz <gmarquez@witapp.com.ar>
2018-08-30 15:05:19 -0300

    Remove unnecessary config

--- api/coinApp/config.py
+++ api/coinApp/config.py
@@ -1,7 +1,6 @@
 # Configuration File
 
 DEBUG = True
-BCRYPT_LOG_ROUNDS = 12 # Configuration for the Flask-Bcrypt extension
 
 SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
 STRIPE_API_KEY = 'SmFjb2IgS2FwbGFuLU1vc3MgaXMgYSBoZXJv'
